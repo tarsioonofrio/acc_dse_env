9fc8c31e4cc44eebee37bc2aafc612220d1642ea
Társio Onofrio <tarsioonofrio@gmail.com>
2023-01-07 14:43:52 -0300

    update

--- apps/lib/generate_files.py
+++ apps/lib/generate_files.py
@@ -377,6 +377,9 @@ def generate_ifmap_vhd_pkg(modelDict, shift, input_size, filter_dimension, filte
 
 def generate_gold_vhd_pkg(modelDict, shift, input_size, filter_dimension, filter_channel, layer_dimension,
                           input_channel, testSet, testLabel, stride_h, stride_w, testSetSize, layer, path):
+    tab = "    "
+    string_pixel = []
+
     # Dataset test variables
     cont_match = 0
     aux_idx_range = 0
@@ -395,7 +398,7 @@ def generate_gold_vhd_pkg(modelDict, shift, input_size, filter_dimension, filter
 
     # string for ORCA gold
     string_orca_gold = " "
-    f = open(path / "gold_pkg.vhd", "w")
+    # f = open(path / "gold_pkg.vhd", "w")
 
     for testCase in range(testSetSize):
         ifmap.clear()
@@ -459,29 +462,41 @@ def generate_gold_vhd_pkg(modelDict, shift, input_size, filter_dimension, filter
                     if layerId == layer:
                         for m in range(layer_dimension[layerId]):
                             for n in range(layer_dimension[layerId]):
-                                if m == 0 and n == 0 and filterId == 0:
-                                    f.write("LIBRARY ieee;\n")
-                                    f.write("USE ieee.std_logic_1164.all;\n")
-                                    f.write("USE ieee.std_logic_signed.all;\n")
-                                    f.write("\tPACKAGE gold_package is\n")
-                                    f.write("\t\ttype padroes is array(0 to 4000000) of integer;\n")
-                                    f.write("\t\tconstant gold: padroes := ( ")
+                                # if m == 0 and n == 0 and filterId == 0:
+                                #     f.write("LIBRARY ieee;\n")
+                                #     f.write("USE ieee.std_logic_1164.all;\n")
+                                #     f.write("USE ieee.std_logic_signed.all;\n")
+                                #     f.write("\tPACKAGE gold_package is\n")
+                                #     f.write("\t\ttype padroes is array(0 to 4000000) of integer;\n")
+                                #     f.write("\t\tconstant gold: padroes := ( ")
                                 if m == 0 and n == 0 and filterId != 0:
-                                    f.write("\t\t")
-                                string_orca_gold = str(int(ofmap[filterId][m][n][0])) + ","
-                                f.write(string_orca_gold)
-                                f.write(" ")
-                            f.write("\n")
-                            f.write("\t\t")
-                        f.write("\n")
+                                    string_pixel.append(tab)
+                                # string_orca_gold = str(int(ofmap[filterId][m][n][0])) + ","
+                                string_orca_gold = str(int(ofmap[filterId][m][n][0]))
+                                string_pixel.append(f"{string_orca_gold}, ")
+                            string_pixel.append(f"\n{tab}")
+                            # string_pixel.append("\n")
+                            # f.write("\t\t")
+                        string_pixel.append(f"\n")
+                        # f.write("\n")
                         # if layerId == layer and filterId == filter_channel[layer] - 1:
 
                 ifmap.append(copy.deepcopy(ofmap))
 
-    f.write("\t\tothers=>0 );\n")
-    f.write("END gold_package;\n")
-    f.close()
+    # f.write("\t\tothers=>0 );\n")
+    # f.write("END gold_package;\n")
+    # f.close()
 
+    with open(Path(__file__).parent.resolve() / "template_inmem_pkg.vhd", "r") as f:
+        text = f.read()
+
+    package = "gold_package"
+    constant = "gold"
+    data = "".join([f"\n{tab}-- gold\n"] + [tab] + string_pixel)
+    text_out = text.format(package=package, constant=constant, data=data)
+
+    with open(path / "gold_pkg.vhd", "w") as f:
+        f.write(text_out)
 
 def generate_ifmem_vhd_pkg(modelDict, shift, input_size, filter_dimension, filter_channel, layer_dimension,
                            input_channel, testSet, testLabel, stride_h, stride_w, testSetSize, layer, path):
