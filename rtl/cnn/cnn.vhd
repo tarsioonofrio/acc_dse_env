13122244f99d9bac285bb554ec531cc2d9542ee4
Társio Onofrio <tarsioonofrio@gmail.com>
2023-01-15 03:57:11 -0300

    ok

--- rtl/cnn/cnn.vhd
+++ rtl/cnn/cnn.vhd
@@ -52,13 +52,13 @@ architecture a1 of cnn is
 
   signal mem_ofmap_valid, mem_ofmap_ce, mem_ofmap_we: std_logic;
 
-  signal debug, start_conv, end_conv, ofmap_valid, ofmap_ce, ofmap_we, iwght_ce, iwght_we, iwght_valid, ifmap_ce, ifmap_we, ifmap_valid: std_logic_vector(0 to 2);
+  signal debug, start_conv, end_conv, ofmap_valid, ofmap_ce, ofmap_we, iwght_valid, ifmap_ce, ifmap_we, ifmap_valid: std_logic_vector(0 to 3);
 
   signal mem_ofmap_address : std_logic_vector(MEM_SIZE-1 downto 0);
-  type type_address is array (0 to 2) of std_logic_vector(MEM_SIZE-1 downto 0);
+  type type_address is array (0 to 3) of std_logic_vector(MEM_SIZE-1 downto 0);
   signal address_in, address_out : type_address;
 
-  type type_value is array (0 to 2) of std_logic_vector(((INPUT_SIZE*2)+CARRY_SIZE)-1 downto 0);
+  type type_value is array (0 to 3) of std_logic_vector(((INPUT_SIZE*2)+CARRY_SIZE)-1 downto 0);
   signal value_out, value_in: type_value;
  
   signal mem_ofmap_in, mem_ofmap_out : std_logic_vector(((INPUT_SIZE*2)+CARRY_SIZE)-1 downto 0);
@@ -73,16 +73,12 @@ architecture a1 of cnn is
 
 begin
 
-  start_conv(0) <= p_start_conv;
-  start_conv(1) <= end_conv(0);
-  start_conv(2) <= end_conv(1);
-  p_end_conv <= end_conv(2);
-
+  end_conv(0) <= p_start_conv;
   ifmap_ce(0) <= p_ifmap_ce;
   ifmap_we(0) <= p_ifmap_we;
   address_in(0) <= p_address;
-  value_in(0) <= p_value_in when p_ifmap_ce = '1' and p_ifmap_we = '1' else value_out(1);
-  p_ifmap_valid <= ifmap_valid(0);
+  value_out(0) <= p_value_in;
+  p_ifmap_valid <= ofmap_valid(0);
 
 
   ifmap_ce(1) <= ofmap_ce(0);
@@ -90,20 +86,30 @@ begin
   address_in(1) <= address_out(0);
   value_in(1) <= value_out(0) when ofmap_ce(0) = '1' and ofmap_we(0) = '1' else value_out(2);
   ofmap_valid(0) <= ifmap_valid(1);
+  start_conv(1) <= end_conv(0);
 
 
   ifmap_ce(2) <= ofmap_ce(1);
   ifmap_we(2) <= ofmap_we(1);
   address_in(2) <= address_out(1);
-  value_in(2) <= value_out(1) when ofmap_ce(1) = '1' and ofmap_we(1) = '1' else mem_ofmap_out;
+  value_in(2) <= value_out(1) when ofmap_ce(1) = '1' and ofmap_we(1) = '1' else value_out(3);
   ofmap_valid(1) <= ifmap_valid(2);
+  start_conv(2) <= end_conv(1);
+
+  ifmap_ce(3) <= ofmap_ce(2);
+  ifmap_we(3) <= ofmap_we(2);
+  address_in(3) <= address_out(2);
+  value_in(3) <= value_out(2) when ofmap_ce(2) = '1' and ofmap_we(2) = '1' else mem_ofmap_out;
+  ofmap_valid(2) <= ifmap_valid(3);
+  start_conv(3) <= end_conv(2);
 
 
-  mem_ofmap_ce <= ofmap_ce(2) or p_ofmap_ce;
-  mem_ofmap_we <= ofmap_we(2) or p_ofmap_we;
-  mem_ofmap_address <= p_address when p_ofmap_ce = '1' else address_out(2);
-  mem_ofmap_in <=  value_out(2);
-  ofmap_valid(2) <= mem_ofmap_valid;
+  mem_ofmap_ce <= ofmap_ce(3) or p_ofmap_ce;
+  mem_ofmap_we <= ofmap_we(3) or p_ofmap_we;
+  mem_ofmap_address <= p_address when p_ofmap_ce = '1' else address_out(3);
+  mem_ofmap_in <=  value_out(3);
+  ofmap_valid(3) <= mem_ofmap_valid;
+  p_end_conv <= end_conv(3);
 
   p_ofmap_valid <= mem_ofmap_valid;
   p_value_out <= mem_ofmap_out;
@@ -128,50 +134,10 @@ begin
       clock         => clock,
       reset         => reset,
 
-      p_start_conv    => start_conv(0),
-      p_end_conv      => end_conv(0),
-      p_debug         => debug(0),
-      config          => config0,
-
-      p_iwght_ce      => '0',
-      p_iwght_we      => '0',
-      p_iwght_valid   => iwght_valid(0),
-
-      p_ifmap_ce      => ifmap_ce(0),
-      p_ifmap_we      => ifmap_we(0),
-      p_ifmap_valid   => ifmap_valid(0),
-
-      p_ofmap_we      => ofmap_we(0),
-      p_ofmap_ce      => ofmap_ce(0),
-      p_ofmap_valid   => ofmap_valid(0),
-
-      p_address_in    => address_in(0),
-      p_value_in      => value_in(0),
-      p_address_out   => address_out(0),
-      p_value_out     => value_out(0)
-      );
-
-  PE1 : entity work.pe
-    generic map(
-      N_FILTER       => N_FILTER,
-      N_CHANNEL      => N_CHANNEL,
-      X_SIZE         => X_SIZE,
-      FILTER_WIDTH   => FILTER_WIDTH,
-      CONVS_PER_LINE => CONVS_PER_LINE,
-      MEM_SIZE       => MEM_SIZE,
-      INPUT_SIZE     => INPUT_SIZE,
-      SHIFT          => SHIFT,
-      CARRY_SIZE     => CARRY_SIZE,
-      IWGHT_PATH     => PATH & "/1/iwght_pkg.txt"
-      )
-    port map(
-      clock         => clock,
-      reset         => reset,
-
       p_start_conv    => start_conv(1),
       p_end_conv      => end_conv(1),
       p_debug         => debug(1),
-      config          => config1,
+      config          => config0,
 
       p_iwght_ce      => '0',
       p_iwght_we      => '0',
@@ -191,7 +157,7 @@ begin
       p_value_out     => value_out(1)
       );
 
-  PEN : entity work.pe
+  PE1 : entity work.pe
     generic map(
       N_FILTER       => N_FILTER,
       N_CHANNEL      => N_CHANNEL,
@@ -229,6 +195,46 @@ begin
       p_value_in      => value_in(2),
       p_address_out   => address_out(2),
       p_value_out     => value_out(2)
+      );
+
+  PEN : entity work.pe
+    generic map(
+      N_FILTER       => N_FILTER,
+      N_CHANNEL      => N_CHANNEL,
+      X_SIZE         => X_SIZE,
+      FILTER_WIDTH   => FILTER_WIDTH,
+      CONVS_PER_LINE => CONVS_PER_LINE,
+      MEM_SIZE       => MEM_SIZE,
+      INPUT_SIZE     => INPUT_SIZE,
+      SHIFT          => SHIFT,
+      CARRY_SIZE     => CARRY_SIZE,
+      IWGHT_PATH     => PATH & "/1/iwght_pkg.txt"
+      )
+    port map(
+      clock         => clock,
+      reset         => reset,
+
+      p_start_conv    => start_conv(3),
+      p_end_conv      => end_conv(3),
+      p_debug         => debug(3),
+      config          => config1,
+
+      p_iwght_ce      => '0',
+      p_iwght_we      => '0',
+      p_iwght_valid   => iwght_valid(3),
+
+      p_ifmap_ce      => ifmap_ce(3),
+      p_ifmap_we      => ifmap_we(3),
+      p_ifmap_valid   => ifmap_valid(3),
+
+      p_ofmap_we      => ofmap_we(3),
+      p_ofmap_ce      => ofmap_ce(3),
+      p_ofmap_valid   => ofmap_valid(3),
+
+      p_address_in    => address_in(3),
+      p_value_in      => value_in(3),
+      p_address_out   => address_out(3),
+      p_value_out     => value_out(3)
       );
 
   OFMAP : entity work.memory
