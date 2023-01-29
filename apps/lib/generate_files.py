198af79dcd232843cd397e8846e5a730cde12afb
Társio Onofrio <tarsioonofrio@gmail.com>
2023-01-28 23:33:12 -0300

    up

--- apps/lib/generate_files.py
+++ apps/lib/generate_files.py
@@ -142,6 +142,7 @@ def generate_samples(input_channel, generic_dict, vhd_dict, layer, path, single_
     generate_vhd = {**vhd_dict, "input_channel": input_channel, "layer":  layer}
     # Generate generic file for rtl simulation
     generate_ifmap_vhd_pkg(path=path_samples, **generate_vhd)
+    generate_gold_vhd_pkg(path=path_samples, **generate_vhd)
     generate_ifmap_bram(path=path_samples, single_file=single_file, bits=generic_dict["MEM_SIZE"], **generate_vhd)
     # Generate VHDL gold output package
     generate_vhd2 = {
