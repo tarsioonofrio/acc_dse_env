f405fff59f1b48f537e9e0036737e4a433a850de
Társio Onofrio <tarsioonofrio@gmail.com>
2023-04-21 18:26:39 -0300

    up

--- rtl/fully_connected/simple.vhd
+++ rtl/fully_connected/simple.vhd
@@ -55,7 +55,7 @@ end entity fully_conected;
 
 
 architecture a1 of fully_conected is
-  signal start_reg, end_reg, ce_ifmap, ce_iwght, ce_ofmap, debug_reg, pipe_reset,en_reg: std_logic;
+  signal iwght_reg, start_reg, end_reg, ce_ifmap, ce_iwght, ce_ofmap, debug_reg, pipe_reset,en_reg: std_logic;
   -- signal add_reg : std_logic_vector(MEM_SIZE-1 downto 0);
 
   signal features : std_logic_vector((INPUT_SIZE*2) - 1 downto 0);
@@ -68,7 +68,6 @@ architecture a1 of fully_conected is
   signal add_iwght : integer range 0 to 2**(MEM_SIZE) - 1;
   signal idx_mac : integer;
   signal idx_wght : integer;
-  signal idx_ifmap : integer;
 
   type type_mac_arr is array (0 to 10-1) of std_logic_vector((INPUT_SIZE*2) - 1 downto 0);
   signal res_mac_arr : type_mac_arr;
@@ -112,34 +111,41 @@ begin
   ofmap_we <= ce_ofmap;
   ofmap_ce <= ce_ofmap;
   ofmap_address <= CONV_STD_LOGIC_VECTOR(add_ofmap, MEM_SIZE);
-  ofmap_out <= const_output & output_reg;
+  -- ofmap_out <= const_output & output_reg;
 
   process(reset, clock)
   begin
     if reset = '1' then
+      pipe_reset <= '1';
       EA_read <= WAITSTART;
-      H <= 0;
-      V <= 0;
-      T <= 0;
-      debug_reg <= '0';
       start_reg <= '0';
+      debug_reg <= '0';
       end_reg <= '0';
       ce_ifmap <= '0';
       ce_iwght <= '0';
       ce_ofmap <= '0';
-      add_reg <= -1;
-      value_reg <= (others => '0'); 
+      add_ifmap <= 0;
+      add_ofmap <= 0;
+      add_iwght <= 0;
+      idx_mac <= 0;
+      idx_wght <= 0;
+      res_mac_arr <= (others => (others => '0')); 
+      reg_mac_arr <= (others => (others => '0')); 
     elsif rising_edge(clock) then
       case EA_read is
         when WAITSTART =>
+          pipe_reset <= '0';
           debug_reg <= '0';
-          add_reg <= -1;
-          debug_reg <= '0';
+          end_reg <= '0';
           ce_ifmap <= '0';
           ce_iwght <= '0';
           ce_ofmap <= '0';
+          add_ifmap <= 0;
+          add_ofmap <= 0;
+          add_iwght <= 0;
+          idx_wght <= 0;
+        
           start_reg <= start_op;
-          
           if start_op = '1' and start_reg = '0' then
             EA_read <= READFEATURES;
           end if;
@@ -156,12 +162,13 @@ begin
         when READWEIGHTS =>
           ce_iwght <= '1';
           idx_mac <= idx_wght;
+          iwght_reg <= iwght_valid;
+          en_reg <= iwght_reg;
           if iwght_valid = '1' then
             weight <= iwght_value;
             if (idx_wght < 10) then -- number of classes
               weight <= iwght_value;
-              add_ifmap <= add_ifmap + P_SIZE;
-              idx_wght <= idx_wght + 1;
+              idx_wght <= idx_wght + 576;
             else
               idx_wght <= 0;
               EA_read <= WAITVALID;
@@ -169,16 +176,10 @@ begin
             end if;
           end if;
         when WAITVALID =>
-          idx_ifmap <= idx_ifmap + 1; -- count number of 
-          add_iwght <= idx_ifmap + 1;
+          add_iwght <= add_ifmap + 1;
         -- report "output: " & integer'image(CONV_INTEGER(signed(value_reg)));
-          output_reg <= value_reg;
-          add_reg <= add_reg + 1;
-          debug_reg <= '1';
-          cw_reg <= '1';
-  
-          if add_wght < X_SIZE*X_SIZE*N_CHANNEL - 1 then -- image max size
-            EA_read <= UPDATEADD;
+          if add_iwght < 576 * 10 then -- image max size
+            EA_read <= READFEATURES;
           else
             EA_read <= WAITSTART;
             end_reg <= '1';
