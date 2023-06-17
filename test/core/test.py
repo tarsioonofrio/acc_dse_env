0f34cf3f2d475ebd7946a30b9501d56214458538
Társio Onofrio <tarsioonofrio@gmail.com>
2023-06-17 07:39:21 -0300

    ok

--- test/core/test.py
+++ test/core/test.py
@@ -34,7 +34,18 @@ async def test(dut):
     conv_time = get_sim_time(units=units)
     conv_steps = get_sim_steps(conv_time, units=units)
 
+    name = os.getenv("MAKEFILE_LIST").strip().split(" ")[0].split(".")[0]
+    cnn = os.getenv("C")
+    rtl = os.getenv("R")
+    root = Path(__file__).parent / "data" / cnn / rtl / name
+    root.mkdir(parents=True, exist_ok=True)
+    layer = os.getenv("L")
+
     data = {
+        "cnn": cnn,
+        "rtl": rtl,
+        "name": name,
+        "layer": layer,
         "units": units,
         "clock_time": clock_time,
         "start_time": start_time,
@@ -46,13 +57,6 @@ async def test(dut):
         "conv_time": conv_time,
         "conv_steps": conv_steps,
     }
-
-    name = os.getenv("MAKEFILE_LIST").strip().split(" ")[0].split(".")[0]
-    cnn = os.getenv("C")
-    rtl = os.getenv("R")
-    root = Path(__file__).parent / "data" / cnn / rtl / name
-    root.mkdir(parents=True, exist_ok=True)
-    layer = os.getenv("L")
     with open(root / f'{layer}.json', 'w', encoding='utf-8') as f:
         json.dump(data, f, ensure_ascii=False, indent=4)
     with open(root / f'{layer}.csv', 'w', encoding='utf-8') as f:
