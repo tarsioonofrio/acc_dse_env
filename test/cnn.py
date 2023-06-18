78bd26c52f7dbdddb9137e08ea82d65a958a9e98
Társio Onofrio <tarsioonofrio@gmail.com>
2023-06-18 00:30:00 -0300

    ok

--- test/cnn.py
+++ test/cnn.py
@@ -18,42 +18,63 @@ async def test(dut):
     await triggers.RisingEdge(dut.clock)
     clock_time = get_sim_time(units=units) - time0
 
-    await triggers.RisingEdge(dut.ifmap_ce)
-    ifmap_time = get_sim_time(units=units)
-    ifmap_steps = get_sim_steps(ifmap_time, units=units)
+    old_time = 0
+    old_steps = 0
 
-    await triggers.RisingEdge(dut.start_conv)
-    start_conv_time = get_sim_time(units=units)
-    start_conv_steps = get_sim_steps(start_conv_time, units=units)
+    # await triggers.RisingEdge(dut.ifmap_ce)
+    # ifmap_time = get_sim_time(units=units)
+    # ifmap_steps = get_sim_steps(ifmap_time, units=units)
+    #
+    # old_time = ifmap_time
+    # old_steps = ifmap_steps
 
-    old_time = start_conv_time
-    old_steps = start_conv_steps
+    # await triggers.RisingEdge(dut.start_conv)
+    # start_conv_time = get_sim_time(units=units)
+    # start_conv_steps = get_sim_steps(start_conv_time, units=units)
 
     layer_data = {
-        'startconvtime': [],
-        'startconvsteps': [],
+        'n': [],
+        'startofmaptime': [],
+        'startofmapsteps': [],
+        'totalofmaptime': [],
+        'totalofmapsteps': [],
+        'endconvtime': [],
+        'endconvsteps': [],
         'totalconvtime': [],
         'totalconvsteps': [],
     }
 
-    for n in range(1, int(dut.N_LAYER)+1):
-        await triggers.RisingEdge(dut.dut.start_conv[n])
-        conv_time = get_sim_time(units=units)
-        conv_steps = get_sim_steps(conv_time, units=units)
-        layer_data['startconvtime'].append(conv_time)
-        layer_data['startconvsteps'].append(conv_steps)
-        layer_data['totalconvtime'].append(conv_time - old_time)
-        layer_data['totalconvsteps'].append(conv_steps - old_steps)
-        old_time = conv_time
-        old_steps = conv_steps
+    for n in range(0, int(dut.N_LAYER)+1):
+        await triggers.RisingEdge(dut.dut.ofmap_ce[n])
+        ofmap_ce_time = get_sim_time(units=units)
+        ofmap_ce_steps = get_sim_steps(ofmap_ce_time, units=units)
+        layer_data['startofmaptime'].append(ofmap_ce_time)
+        layer_data['startofmapsteps'].append(ofmap_ce_steps)
+
+        await triggers.RisingEdge(dut.dut.end_conv[n])
+        end_conv_time = get_sim_time(units=units)
+        end_conv_steps = get_sim_steps(end_conv_time, units=units)
+
+        layer_data['totalofmaptime'].append(end_conv_time - ofmap_ce_time)
+        layer_data['totalofmapsteps'].append(end_conv_steps - ofmap_ce_steps)
+
+        layer_data['n'].append(n)
+        layer_data['endconvtime'].append(end_conv_time)
+        layer_data['endconvsteps'].append(end_conv_steps)
+        layer_data['totalconvtime'].append(end_conv_time - old_time)
+        layer_data['totalconvsteps'].append(end_conv_steps - old_steps)
+        old_time = end_conv_time
+        old_steps = end_conv_steps
 
         # await triggers.RisingEdge(dut.dut.ifmap_ce[n])
         # ifmap_time = get_sim_time(units=units)
         # ifmap_steps = get_sim_steps(ifmap_time, units=units)
 
-    await triggers.RisingEdge(dut.end_conv)
-    end_cnn_time = get_sim_time(units=units)
-    end_cnn_steps = get_sim_steps(end_cnn_time, units=units)
+    # await triggers.RisingEdge(dut.end_conv)
+    start_cnn_time = layer_data['endconvtime'][0]
+    start_cnn_steps = layer_data['endconvsteps'][0]
+    end_cnn_time = layer_data['endconvtime'][-1]
+    end_cnn_steps = layer_data['endconvsteps'][-1]
 
     name = os.getenv("MAKEFILE_LIST").strip().split(" ")[0].split(".")[0]
     cnn = os.getenv("C")
@@ -68,25 +89,27 @@ async def test(dut):
         "name": name,
         "units": units,
         "clocktime": clock_time,
-        "starttime": ifmap_time,
-        "startsteps": ifmap_steps,
-        "startifmaptime": start_conv_time,
-        "startifmapsteps": start_conv_steps,
-        "totalifmaptime": start_conv_time - ifmap_time,
-        "totalifmapsteps": start_conv_steps - ifmap_steps,
+        # "starttime": ifmap_time,
+        # "startsteps": ifmap_steps,
+        # "startifmaptime": ifmap_time,
+        # "startifmapsteps": ifmap_steps,
+        # "totalifmaptime": start_cnn_time - ifmap_time,
+        # "totalifmapsteps": start_cnn_steps - ifmap_steps,
+        "startcnntime": start_cnn_time,
+        "startcnnsteps": start_cnn_steps,
         "endcnntime": end_cnn_time,
         "endcnnsteps": end_cnn_steps,
-        "totalcnntime": end_cnn_time - start_conv_time,
-        "totalcnnsteps": end_cnn_steps - start_conv_steps,
+        "totalcnntime": end_cnn_time - start_cnn_time,
+        "totalcnnsteps": end_cnn_steps - start_cnn_steps,
     }
 
     # print("dut", dir(dut))
     # print("dut.cnn", dir(dut.dut))
     # print("dut.cnn.start_conv", len(dut.dut.start_conv))
 
-    with open(root / f'model.json', 'w', encoding='utf-8') as f:
+    with open(root / f'total.json', 'w', encoding='utf-8') as f:
         json.dump(data, f, ensure_ascii=False, indent=4)
-    with open(root / f'model.csv', 'w', encoding='utf-8') as f:
+    with open(root / f'total.csv', 'w', encoding='utf-8') as f:
         wr = csv.writer(f, quoting=csv.QUOTE_MINIMAL)
         wr.writerow(list(data.keys()))
         wr.writerow(list(data.values()))
