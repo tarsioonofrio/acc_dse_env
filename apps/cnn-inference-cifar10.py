460ab93be15d202a954672f42662b0eac9585b62
Társio Onofrio <tarsioonofrio@gmail.com>
2023-06-19 04:07:19 -0300

    ok

--- apps/cnn-inference-cifar10.py
+++ apps/cnn-inference-cifar10.py
@@ -27,7 +27,7 @@ all_paths = cnn_output_path.glob('**/weights')
 
 benchmark1 = {}
 benchmark10 = {}
-benchmark100 = {}
+# benchmark100 = {}
 for path in cnn_output_path.glob('**/weights'):
     # Clearup everything before running
     keras.backend.clear_session()
@@ -37,7 +37,7 @@ for path in cnn_output_path.glob('**/weights'):
     name = path.parent.name
     benchmark1[name] = []
     benchmark10[name] = []
-    benchmark100[name] = []
+    # benchmark100[name] = []
 
     # warmup
     for i in range(1, 10):
@@ -55,16 +55,16 @@ for path in cnn_output_path.glob('**/weights'):
         end = time.time_ns()
         benchmark10[name].append(end - start)
 
-    for i in range(1, 10):
-        start = time.time_ns()
-        model.predict(x_test[:100], batch_size=100)
-        end = time.time_ns()
-        benchmark100[name].append(end - start)
+    # for i in range(1, 10):
+    #     start = time.time_ns()
+    #     model.predict(x_test[:100], batch_size=100)
+    #     end = time.time_ns()
+    #     benchmark100[name].append(end - start)
 
 df1 = pd.DataFrame.from_dict(benchmark1).mean()
 df10 = pd.DataFrame.from_dict(benchmark10).mean() / 10
-df100 = pd.DataFrame.from_dict(benchmark100).mean() / 100
+# df100 = pd.DataFrame.from_dict(benchmark100).mean() / 100
 
 m1 = pd.merge(df1.rename('batch1'), df10.rename('batch10'), left_index=True, right_index=True)
-m = pd.merge(m1, df100.rename('batch100'), left_index=True, right_index=True)
-m.to_csv(root.parent / f"test/report/{device}.csv")
+# m = pd.merge(m1, df100.rename('batch100'), left_index=True, right_index=True)
+m1.to_csv(root.parent / f"test/report/{device}.csv")
