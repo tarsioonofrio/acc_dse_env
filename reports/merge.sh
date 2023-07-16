d753a761665e3944c4cbc420f275f0b267aedbe4
Társio Onofrio <tarsioonofrio@gmail.com>
2023-07-15 21:40:42 -0300

    ok

--- reports/merge.sh
+++ reports/merge.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-mkdir -p table
-rm -f table/*.xlsx
+mkdir -p benchmark
+rm -f benchmark/*.xlsx
 
-find data/basic/ -name "*.csv" -exec awk 'FNR>1 || NR==1' {} + > table/basic.csv
-find data/core/ -name "*.csv" -exec awk 'FNR>1 || NR==1' {} + > table/core.csv
-find data/core_serial_input/ -name "*.csv" -exec awk 'FNR>1 || NR==1' {} + > table/core_serial_input.csv
-find data/cnn/ -name "total.csv" -exec awk 'FNR>1 || NR==1' {} + > table/cnn-model.csv
-find data/cnn/ -name "layer.csv" -exec awk 'FNR>1 || NR==1' {} + > table/cnn-layer.csv
+find ../data/basic/ -name "*.csv" -exec awk 'FNR>1 || NR==1' {} + > benchmark/basic.csv
+find ../data/core/ -name "*.csv" -exec awk 'FNR>1 || NR==1' {} + > benchmark/core.csv
+find ../data/core_serial_input/ -name "*.csv" -exec awk 'FNR>1 || NR==1' {} + > benchmark/core_serial_input.csv
+find ../data/cnn/ -name "total.csv" -exec awk 'FNR>1 || NR==1' {} + > benchmark/cnn-model.csv
+find ../data/cnn/ -name "layer.csv" -exec awk 'FNR>1 || NR==1' {} + > benchmark/cnn-layer.csv
 
-./csv2xlsx -o table/basic.xlsx table/basic.csv
-./csv2xlsx -o table/core.xlsx table/core.csv
-./csv2xlsx -o table/core_serial_input.xlsx table/core_serial_input.csv
-./csv2xlsx -o table/cnn-model.xlsx table/cnn-model.csv
-./csv2xlsx -o table/cnn-layer.xlsx table/cnn-layer.csv
+./csv2xlsx -o benchmark/basic.xlsx benchmark/basic.csv
+./csv2xlsx -o benchmark/core.xlsx benchmark/core.csv
+./csv2xlsx -o benchmark/core_serial_input.xlsx benchmark/core_serial_input.csv
+./csv2xlsx -o benchmark/cnn-model.xlsx benchmark/cnn-model.csv
+./csv2xlsx -o benchmark/cnn-layer.xlsx benchmark/cnn-layer.csv
