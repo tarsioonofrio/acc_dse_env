e146645f9a7d81eb205b4a8967edaf966a502b08
Társio Onofrio <tarsioonofrio@gmail.com>
2023-07-23 19:35:25 -0300

    ok

--- apps/rtl-generate.py
+++ apps/rtl-generate.py
@@ -6,53 +6,48 @@ import argparse
 from pathlib import Path
 from types import SimpleNamespace
 
-import torch
-import numpy as np
+# import torch
 
-from lib import util, keras_cifar10
-# from lib.generate_files import (
-#     generate_files, generate_generic_file, generate_tcl_generic, generate_config_file,
-#     generate_samples, generate_gold_maxpool_vhd_pkg, fc, dict_op_type
-# )
+from lib import keras_cifar10
 from lib.generate_files import GenerateRTL
 from lib.model import dictionary_from_model
-from lib.pytorch_models import Default
+# from lib.pytorch_models import Default
+
 
 def main():
     parser = argparse.ArgumentParser(
         usage='use "python %(prog)s --help" for more information.\n'
     )
-    parser.add_argument("--cnn_config", "-c", type=str, help="Name of neural network config file in nn_config")
+    # parser.add_argument("--cnn_config", "-c", type=str, help="Name of neural network config file in nn_config")
     parser.add_argument("--rtl_config", "-r", type=str, help="Name of hardware config file in rtl_config")
     args = parser.parse_args()
 
     root = Path(__file__).parent.resolve()
-    cnn_config_path = root / "cnn_config" / f"{args.cnn_config}.json"
+    # cnn_config_path = root / "cnn_config" / f"{args.cnn_config}.json"
     cnn_output_path = root / "cnn_output" / args.cnn_config
     rtl_config_path = root / "rtl_config" / f"{args.rtl_config}.json"
     rtl_output_path = root / "rtl_output" / args.cnn_config / args.rtl_config
 
     rtl_output_path.mkdir(parents=True, exist_ok=True)
 
-    with open(cnn_config_path) as f:
-        cnn_config = json.load(f)
+    # with open(cnn_config_path) as f:
+    #     cnn_config = json.load(f)
 
     with open(rtl_config_path) as f:
         rtl_config = json.load(f)
 
-    shift_bits = rtl_config["INPUT_SIZE"] / 2
+    # shift_bits = rtl_config["INPUT_SIZE"] / 2
     # Adjust shift
-    shift = 2 ** shift_bits
-
-    # Compute number of convolutional layers
-    config_dataset = {
-        "input_w": 32,
-        "input_h": 32,
-        "input_c": 3,
-        "classes": 10,
-    }
-    model = Default(cnn_config, config_dataset)
-    model.load_state_dict(torch.load(cnn_output_path / 'model.pth'))
+    # shift = 2 ** shift_bits
+    # # Compute number of convolutional layers
+    # config_dataset = {
+    #     "input_w": 32,
+    #     "input_h": 32,
+    #     "input_c": 3,
+    #     "classes": 10,
+    # }
+    # model = Default(cnn_config, config_dataset)
+    # model.load_state_dict(torch.load(cnn_output_path / 'model.pth'))
 
     if os.path.exists(cnn_output_path / "weights.pkl"):
         # model_dict = {int(k): v for k, v in loadmat(str(path / "model.mat")).items() if k[0] != '_'}
@@ -60,7 +55,6 @@ def main():
             model_dict = pickle.load(f)
     else:
         from tensorflow import keras
-        from lib import keras_models
         model = keras.models.load_model(cnn_output_path / "weights")
         # Generate dictionary
         model_dict = dictionary_from_model(model)
