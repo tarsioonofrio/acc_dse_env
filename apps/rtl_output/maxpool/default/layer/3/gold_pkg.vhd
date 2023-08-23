d7de725a41b5aab8fbe09523a4a673c52b8bc3fb
Társio Onofrio <5504075+tarsioonofrio@users.noreply.github.com>
2023-08-23 02:36:12 -0300

    maxpool almost ok

--- apps/rtl_output/maxpool/default/layer/3/gold_pkg.vhd
+++ apps/rtl_output/maxpool/default/layer/3/gold_pkg.vhd
@@ -6,7 +6,199 @@ package gold_package is
 
   constant gold : mem := (
 
-    -- ifmap
-    292, 555, 0, 277, 0, 94, 1015, 289, 0, 0, 151, 764, 307, 328, 315, 0, 79, 157, 0, 267, 303, 185, 172, 0, 358, 0, 519, 164, 200, 0, 53, 207, 0, 130, 668, 65, 421, 804, 0, 0, 384, 90, 83, 164, 0, 0, 315, 276, 383, 13, 562, 0, 436, 186, 0, 521, 448, 0, 417, 16, 279, 359, 0, 164,
+    -- gold
+    -- channel=0
+    292, 
+    
+    -- channel=1
+    555, 
+    
+    -- channel=2
+    0, 
+    
+    -- channel=3
+    277, 
+    
+    -- channel=4
+    0, 
+    
+    -- channel=5
+    94, 
+    
+    -- channel=6
+    1015, 
+    
+    -- channel=7
+    289, 
+    
+    -- channel=8
+    0, 
+    
+    -- channel=9
+    0, 
+    
+    -- channel=10
+    151, 
+    
+    -- channel=11
+    764, 
+    
+    -- channel=12
+    307, 
+    
+    -- channel=13
+    328, 
+    
+    -- channel=14
+    315, 
+    
+    -- channel=15
+    0, 
+    
+    -- channel=16
+    79, 
+    
+    -- channel=17
+    157, 
+    
+    -- channel=18
+    0, 
+    
+    -- channel=19
+    267, 
+    
+    -- channel=20
+    303, 
+    
+    -- channel=21
+    185, 
+    
+    -- channel=22
+    172, 
+    
+    -- channel=23
+    0, 
+    
+    -- channel=24
+    358, 
+    
+    -- channel=25
+    0, 
+    
+    -- channel=26
+    519, 
+    
+    -- channel=27
+    164, 
+    
+    -- channel=28
+    200, 
+    
+    -- channel=29
+    0, 
+    
+    -- channel=30
+    53, 
+    
+    -- channel=31
+    207, 
+    
+    -- channel=32
+    0, 
+    
+    -- channel=33
+    130, 
+    
+    -- channel=34
+    668, 
+    
+    -- channel=35
+    65, 
+    
+    -- channel=36
+    421, 
+    
+    -- channel=37
+    804, 
+    
+    -- channel=38
+    0, 
+    
+    -- channel=39
+    0, 
+    
+    -- channel=40
+    384, 
+    
+    -- channel=41
+    90, 
+    
+    -- channel=42
+    83, 
+    
+    -- channel=43
+    164, 
+    
+    -- channel=44
+    0, 
+    
+    -- channel=45
+    0, 
+    
+    -- channel=46
+    315, 
+    
+    -- channel=47
+    276, 
+    
+    -- channel=48
+    383, 
+    
+    -- channel=49
+    13, 
+    
+    -- channel=50
+    562, 
+    
+    -- channel=51
+    0, 
+    
+    -- channel=52
+    436, 
+    
+    -- channel=53
+    186, 
+    
+    -- channel=54
+    0, 
+    
+    -- channel=55
+    521, 
+    
+    -- channel=56
+    448, 
+    
+    -- channel=57
+    0, 
+    
+    -- channel=58
+    417, 
+    
+    -- channel=59
+    16, 
+    
+    -- channel=60
+    279, 
+    
+    -- channel=61
+    359, 
+    
+    -- channel=62
+    0, 
+    
+    -- channel=63
+    164, 
+    
+    
     others => 0);
 end gold_package;
