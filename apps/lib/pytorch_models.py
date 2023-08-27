e1caf8fb19878a98461c52be55e4102b4c9f4040
Társio Onofrio <5504075+tarsioonofrio@users.noreply.github.com>
2023-08-26 22:14:00 -0300

    ok

--- apps/lib/pytorch_models.py
+++ apps/lib/pytorch_models.py
@@ -28,7 +28,7 @@ class Default(nn.Module):
             nn.ReLU(),
             *conv,
             nn.Flatten(1, -1),
-            nn.Linear(64*9, 10),
+            nn.Linear(config_model["filter_channel"][2]*9, 10),
             nn.Softmax(1),
         )
 
@@ -69,7 +69,7 @@ class MaxPool(nn.Module):
             *conv,
             nn.MaxPool2d(kernel_size=3),
             nn.Flatten(1, -1),
-            nn.Linear(64, 10),
+            nn.Linear(config_model["filter_channel"][2], 10),
             nn.Softmax(1),
         )
 
@@ -110,7 +110,51 @@ class BatchNorm(nn.Module):
             nn.ReLU(),
             *conv,
             nn.Flatten(1, -1),
-            nn.Linear(64*9, 10),
+            nn.Linear(config_model["filter_channel"][2]*9, 10),
+            nn.Softmax(1),
+        )
+
+    def forward(self, x):
+        if self.debug:
+            for layer in self.sequential:
+                x = layer(x)
+        else:
+            x = self.sequential(x)
+        return x
+
+
+class Default3FC(nn.Module):
+    def __init__(self, config_model, debug=False):
+        super().__init__()
+        self.debug = debug
+        conv0 = nn.Conv2d(
+            in_channels=3, out_channels=config_model["filter_channel"][0],
+            kernel_size=(config_model["filter_dimension"][0], config_model["filter_dimension"][0]),
+            stride=(config_model["stride_h"][0], config_model["stride_w"][0])
+        )
+        conv = [
+            layer
+            for i in range(1, len(config_model["filter_channel"]))
+            for layer in [
+                nn.Conv2d(
+                    in_channels=config_model["filter_channel"][i - 1], out_channels=config_model["filter_channel"][i],
+                    kernel_size=(config_model["filter_dimension"][i], config_model["filter_dimension"][i]),
+                    stride=(config_model["stride_h"][i], config_model["stride_w"][i])
+                ),
+                nn.ReLU()
+            ]
+        ]
+
+        self.sequential = nn.Sequential(
+            conv0,
+            nn.ReLU(),
+            *conv,
+            nn.Flatten(1, -1),
+            nn.Linear(config_model["filter_channel"][2]*9, config_model["filter_channel"][2]*9),
+            nn.ReLU(),
+            nn.Linear(config_model["filter_channel"][2]*9, config_model["filter_channel"][2]*9),
+            nn.ReLU(),
+            nn.Linear(config_model["filter_channel"][2]*9, 10),
             nn.Softmax(1),
         )
 
