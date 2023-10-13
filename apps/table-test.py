205d5fe9b4d8638ff23cd1ae7532f4a6033848c3
Társio Onofrio <5504075+tarsioonofrio@users.noreply.github.com>
2023-10-12 22:15:12 -0300

    ok

--- apps/table-test.py
+++ apps/table-test.py
@@ -1,4 +1,9 @@
+import json
+import argparse
+from pathlib import Path
+
 import numpy as np
+import pandas as pd
 
 
 class Systolic2dWs:
@@ -58,13 +63,31 @@ class Systolic2dWs:
         in_sum = np.zeros(3, dtype=int)
         in_h = 0
         in_v = 0
+
+        report_arr = {
+            f'{n}{s}': []
+            for n, d in zip(['add', 'mac', 'sum'], [in_add, in_mac, in_sum])
+            for s in range(d.size)
+        }
+        report = {
+            'h': [],
+            'v': [],
+            **report_arr
+        }
         for i in range(-1, time):
             print('time', i, time)
             out_add, out_mac, out_sum, out_h, out_v = self(in_add, in_mac, in_sum, in_h, in_v)
+            for n, d in zip(['add', 'mac', 'sum'], [in_add, in_mac, in_sum]):
+                for s in range(d.size):
+                    report[f'{n}{s}'].append(d.reshape(-1)[s])
+            report['h'].append(out_h)
+            report['v'].append(out_v)
             in_add, in_mac, in_sum, in_h, in_v = np.copy(out_add), np.copy(out_mac), np.copy(out_sum), out_h, out_v
+        df = pd.DataFrame.from_dict(report).T
+        return df
 
 
-def main():
+def test():
     x_size = 5
     filter_width = 3
     convs_per_line = 3
@@ -89,7 +112,52 @@ def main():
     ])
 
     conv = Systolic2dWs(features, weight, bias, x_size)
-    conv.sim(15)
+    report = conv.sim(15)
+    print()
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        usage='use "python %(prog)s --help" for more information.\n'
+    )
+    parser.add_argument("--cnn_config", "-c", type=str, help="Name of neural network config file in nn_config")
+    parser.add_argument("--rtl_config", "-r", type=str, help="Name of hardware config file in rtl_config")
+    parser.add_argument("--layer", "-l", type=str, help="")
+    args = parser.parse_args()
+
+    root = Path(__file__).parent.parent.resolve() / 'experiments'
+    cnn_config_path = root / "cnn_config" / f"{args.cnn_config}.json"
+    cnn_output_path = root / "cnn_output" / args.cnn_config
+    rtl_config_path = root / "rtl_config" / f"{args.rtl_config}.json"
+    rtl_output_path = root / "rtl_output" / args.cnn_config / args.rtl_config
+    output_path = root / "rtl_output" / args.cnn_config / args.rtl_config / 'test_table'
+    output_path.mkdir(parents=True, exist_ok=True)
+
+    with open(cnn_config_path) as f:
+        cnn_config = json.load(f)
+
+    with open(rtl_config_path) as f:
+        rtl_config = json.load(f)
+
+    weight_path = root / "rtl_output" / args.cnn_config / args.rtl_config / 'layer' / args.layer / 'iwght.txt'
+    fmap_path = root / "rtl_output" / args.cnn_config / args.rtl_config / 'layer' / args.layer / 'ifmap.txt'
+
+    with open(weight_path) as f:
+        weight_bias_data = [int(i) for i in f.read().split("\n") if i != '']
+
+        with open(fmap_path) as f:
+            features = np.array([int(i) for i in f.read().split("\n") if i != ''])
+
+    layer = int(args.layer)
+    n_filter = cnn_config["filter_channel"][layer]
+    weight_bias = np.array(weight_bias_data)
+    bias = weight_bias[layer]
+    weight = weight_bias[n_filter:].reshape(-1, 3, 3)[layer]
+
+    x_size = 32
+    conv = Systolic2dWs(features, weight, bias, x_size)
+    df = conv.sim(15)
+    df.to_csv(output_path / f'{layer}.csv')
     print()
 
 
