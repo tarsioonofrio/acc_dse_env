b95b42e427109b7672557eebbc5be3aa33a09158
Társio Onofrio <5504075+tarsioonofrio@users.noreply.github.com>
2023-10-25 15:47:57 -0300

    ok

--- apps/table-test2.py
+++ apps/table-test2.py
@@ -6,89 +6,9 @@ from pathlib import Path
 
 import numpy as np
 import pandas as pd
-from pyDigitalWaveTools.vcd.parser import VcdParser
 
 
-class Systolic2dWs:
-    def __init__(self, features, weight, bias, x_size):
-        self.features = features
-        self.weight = weight
-        self.bias = bias
-        self.x_size = x_size
-
-    def __call__(self, in_add, in_mac, in_sum, in_h, in_v):
-        out_mac = np.zeros_like(in_mac)
-        out_add = np.zeros_like(in_add)
-        out_sum = np.zeros_like(in_sum)
-
-        if in_add[-1] < self.x_size * self.x_size:
-            out_mac[0][0] = self.features[in_add[0]] * self.weight[0][0]
-            out_mac[0][1] = self.features[in_add[0]] * self.weight[0][1] + in_mac[0][0]
-            out_mac[0][2] = self.features[in_add[0]] * self.weight[0][2] + in_mac[0][1]
-
-            out_mac[1][0] = self.features[in_add[1]] * self.weight[1][0]
-            out_mac[1][1] = self.features[in_add[1]] * self.weight[1][1] + in_mac[1][0]
-            out_mac[1][2] = self.features[in_add[1]] * self.weight[1][2] + in_mac[1][1]
-
-            out_mac[2][0] = self.features[in_add[2]] * self.weight[2][0]
-            out_mac[2][1] = self.features[in_add[2]] * self.weight[2][1] + in_mac[2][0]
-            out_mac[2][2] = self.features[in_add[2]] * self.weight[2][2] + in_mac[2][1]
-
-        out_sum[0] = in_mac[0][2]
-        out_sum[1] = in_mac[1][2] + in_sum[0]
-        out_sum[2] = in_mac[2][2] + in_sum[1]
-
-        if in_h + 1 >= self.x_size:
-            out_h = 0
-            out_v = in_v + self.x_size
-        else:
-            out_h = in_h + 1
-            out_v = in_v
-
-        out_add[0] = in_v + in_h
-        out_add[1] = self.x_size + in_add[0]
-        out_add[2] = self.x_size + in_add[1]
-
-        # print('add', in_add)
-        # print('mac[0]', out_mac[0])
-        # print('mac[1]', out_mac[1])
-        # print('mac[2]', out_mac[2])
-        # print('sum[0]', out_sum[0])
-        # print('sum[1]', out_sum[1])
-        # print('sum[2]', out_sum[2])
-
-        return out_add, out_mac, out_sum, out_h, out_v
-
-    def sim(self, time):
-        in_add = np.array([0, 0, 0])
-        in_mac = np.zeros((3, 3), dtype=int)
-        in_sum = np.zeros(3, dtype=int)
-        in_h = 0
-        in_v = 0
-
-        report_arr = {
-            f'{n}{s}': []
-            for n, d in zip(['add', 'mac', 'sum'], [in_add, in_mac, in_sum])
-            for s in range(d.size)
-        }
-        report = {
-            'h': [],
-            'v': [],
-            **report_arr
-        }
-        for i in range(-1, time):
-            out_add, out_mac, out_sum, out_h, out_v = self(in_add, in_mac, in_sum, in_h, in_v)
-            for n, d in zip(['add', 'mac', 'sum'], [in_add, in_mac, in_sum]):
-                for s in range(d.size):
-                    report[f'{n}{s}'].append(d.reshape(-1)[s])
-            report['h'].append(out_h)
-            report['v'].append(out_v)
-            in_add, in_mac, in_sum, in_h, in_v = np.copy(out_add), np.copy(out_mac), np.copy(out_sum), out_h, out_v
-        df = pd.DataFrame.from_dict(report).T
-        return df
-
-
-def convolution2d(features, weights, stride, padding):
+def convolution2d(features, weights, gold, n_sim, stride=1, padding=0):
     features = np.pad(features, [(0, 0), (padding, padding), (padding, padding)], mode='constant', constant_values=0)
 
     weights_channel, _, weight_height, weight_width = weights.shape
@@ -126,13 +46,11 @@ def convolution2d(features, weights, stride, padding):
                     ]
                     temp_add[wc, fy, fx] = np.array(tmp).reshape(output_height, output_width)
 
-    nsim = 19
-
-    reg_mac = np.zeros((nsim, weight_height, weight_width), dtype=int)
-    reg_sum = np.zeros((nsim, weight_width), dtype=int)
-    features_sim = np.zeros((nsim, weight_width), dtype=int)
-    add = np.zeros((nsim, weight_height), dtype=int)
-    weights_sim = np.zeros(nsim, dtype=int)
+    reg_mac = np.zeros((n_sim, weight_height, weight_width), dtype=int)
+    reg_sum = np.zeros((n_sim, weight_width), dtype=int)
+    features_sim = np.zeros((n_sim, weight_width), dtype=int)
+    add = np.zeros((n_sim, weight_height), dtype=int)
+    weights_sim = np.zeros(n_sim, dtype=int)
     for wc in range(0, output_channel):
         for fy in range(0, output_height):
             stride_space = fy * 2
@@ -150,14 +68,18 @@ def convolution2d(features, weights, stride, padding):
     report_arr = {
         f'{n}{e}': data
         for n, array in zip(['add', 'ftr', 'mac', 'sum', ], [add, features_sim, reg_mac, reg_sum])
-        for e, data in enumerate(array.reshape(nsim, -1).swapaxes(0, 1).tolist())
+        for e, data in enumerate(array.reshape(n_sim, -1).swapaxes(0, 1).tolist())
     }
     report = {
         **report_arr, 'wid': weights_sim
     }
     df = pd.DataFrame.from_dict(report)
     df = df.reset_index(drop=True)
-    return output_features, df
+    output_features[output_features < 0] = 0
+    output_features = output_features.reshape(-1)
+    cmp = gold == output_features
+    assert np.all(cmp)
+    return df
 
 
 def basic():
@@ -176,6 +98,12 @@ def basic():
         [5, 3, -7],
     ]]])
 
+    gold = np.array([
+        0, 5, 0,
+        92, 37, 94,
+        84, 0, 138,
+    ])
+
     features = np.array([[
         -1, 3, -10, 8, -4,
         -4, 9, 5, -2, 4,
@@ -184,12 +112,10 @@ def basic():
         1, 2, 3, 4, 5,
     ]])
 
-    n_sim = x_size * (x_size-2) + 3 + 1
-    n_sim2 = convs_per_line * (convs_per_line + 2) + 3 + 1
-    conv = Systolic2dWs(features[0], weight[0, 0], bias, x_size)
-    report = conv.sim(n_sim)
-    report.to_csv(f'../../test.csv')
-    output, report = convolution2d(features=features.reshape(-1, x_size, x_size), weights=weight, stride=1, padding=0)
+    n_sim = convs_per_line * (convs_per_line + 2) + 3 + 1
+    report = convolution2d(
+        features=features.reshape((-1, x_size, x_size)), weights=weight, gold=gold, n_sim=n_sim, stride=1, padding=0
+    )
     report.to_csv(f'../../test2.csv')
     print()
 
