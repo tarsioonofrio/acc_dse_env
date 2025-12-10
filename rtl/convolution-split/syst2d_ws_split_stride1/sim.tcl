9b61adccc5f98fb5f952e12d6eb935c921d2379b
Társio Onofrio <5504075+tarsioonofrio@users.noreply.github.com>
2025-12-10 00:35:03 -0300

    Update sim.tcl

--- rtl/convolution-split/syst2d_ws_split_stride1/sim.tcl
+++ rtl/convolution-split/syst2d_ws_split_stride1/sim.tcl
@@ -25,32 +25,32 @@ vcom -work work ../tb_rtl_split.vhd
 
 # Simulation
 vsim -voptargs=+acc=lprn -t ps work.tb -f ../../../experiments/rtl_output/$CNN/$RTL/layer/0/generic_file.txt
-#do wave_syst2d_ws.do
+do wave.do
 #onfinish exit
 #onbreak exit
 # log -r /*
 
-add wave sim:/tb/*
-add wave sim:/tb/DUT/*
-add wave sim:/tb/clock
-add wave sim:/tb/DUT/cont_total_valid
-add wave sim:/tb/DUT/cont_valid
-add wave sim:/tb/DUT/cont_conv
-add wave sim:/tb/DUT/read_weight_flag
-add wave sim:/tb/DUT/EA_read
-add wave sim:/tb/DUT/iwght_valid
-add wave sim:/tb/iwght_ce
-add wave sim:/tb/DUT/cont_weight_cycles
-add wave sim:/tb/DUT/weight_control
+# add wave sim:/tb/*
+# add wave sim:/tb/DUT/*
+# add wave sim:/tb/clock
+# add wave sim:/tb/DUT/cont_total_valid
+# add wave sim:/tb/DUT/cont_valid
+# add wave sim:/tb/DUT/cont_conv
+# add wave sim:/tb/DUT/read_weight_flag
+# add wave sim:/tb/DUT/EA_read
+# add wave sim:/tb/DUT/iwght_valid
+# add wave sim:/tb/iwght_ce
+# add wave sim:/tb/DUT/cont_weight_cycles
+# add wave sim:/tb/DUT/weight_control
 
-add wave sim:/tb/DUT/add
-add wave sim:/tb/DUT/ofmap_address
-add wave sim:/tb/DUT/ofmap_out
-add wave sim:/tb/DUT/reg_soma1
-add wave sim:/tb/DUT/reg_soma2
-add wave sim:/tb/DUT/reg_soma3
-add wave sim:/tb/DUT/bias_x
-add wave sim:/tb/DUT/cont_weight_cycles
+# add wave sim:/tb/DUT/add
+# add wave sim:/tb/DUT/ofmap_address
+# add wave sim:/tb/DUT/ofmap_out
+# add wave sim:/tb/DUT/reg_soma1
+# add wave sim:/tb/DUT/reg_soma2
+# add wave sim:/tb/DUT/reg_soma3
+# add wave sim:/tb/DUT/bias_x
+# add wave sim:/tb/DUT/cont_weight_cycles
 
 run -all
 #run 1000 ns
