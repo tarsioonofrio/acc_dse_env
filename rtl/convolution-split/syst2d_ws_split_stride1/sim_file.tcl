de25daac02c58a331187f7e8e79118bc9606c694
Társio Onofrio <5504075+tarsioonofrio@users.noreply.github.com>
2026-01-15 20:49:17 -0300

    Add syst2d WS stride-1 testbench and sim script

---
+++ rtl/convolution-split/syst2d_ws_split_stride1/sim_file.tcl
@@ -0,0 +1,45 @@
+if {[file isdirectory work]} { vdel -all -lib work }
+vlib work
+vmap work work
+
+set CNN conv1-3x3
+set RTL default
+set LAYER 0
+set LAYER_PATH "../../../experiments/rtl_output/$CNN/$RTL/layer/$LAYER"
+set GENERIC_FILE "$LAYER_PATH/generic_file.txt"
+set PATH "/home/tarsio/gaph/FastConv_SystemVerilog/data/ifn9/sim/sim-032-3-3-normal"
+
+# Packages for CNN layer simulation
+# vcom -work work $LAYER_PATH/ifmap_pkg.vhd
+# vcom -work work $LAYER_PATH/iwght_pkg.vhd
+# vcom -work work $LAYER_PATH/gold_pkg.vhd
+
+# Components
+vcom -work work ../../components/mac/mac.vhd
+vcom -work work ../../components/reg/reg.vhd
+vcom -work work ../../components/mem_file/mem_file.vhd
+
+# Convolution core
+vcom -work work syst2d_ws_split_stride1.vhd
+
+# Testbench
+vcom -work work tb_syst2d_ws_split_stride1.vhd
+
+# Simulation
+set fh [open $GENERIC_FILE r]
+set generic_line [read $fh]
+close $fh
+
+set generic_args [split $generic_line]
+set filtered {}
+foreach arg $generic_args {
+  if {[string match "-gPATH=*" $arg]} {
+    continue
+  }
+  lappend filtered $arg
+}
+
+vsim -voptargs=+acc=lprn -t ps work.tb -gPATH=$PATH {*}$filtered
+do wave.do
+
+run -all
---
+++ rtl/convolution-split/syst2d_ws_split_stride1/tb_syst2d_ws_split_stride1_file.vhd
@@ -0,0 +1,260 @@
+library ieee;
+library std;
+
+use ieee.std_logic_1164.all;
+use ieee.std_logic_signed.all;
+use IEEE.std_logic_arith.all;
+use ieee.std_logic_textio.all;
+
+use std.textio.all;
+
+use work.gold_package.all;
+
+
+entity tb is
+  generic (
+    ARRAY_TYPE     : string := "syst2d";
+    CARRY_SIZE     : integer := 4;
+    CLK_HALF       : time := 1.0 ns;
+    CLK_PERIOD     : time := 2.0 ns;
+    CONVS_PER_LINE : integer := 15;
+    DATAFLOW_TYPE  : string := "ws";
+    DEVICE         : string := "7SERIES";
+    FALL_START     : time := 0 ns;
+    FILTER_WIDTH   : integer := 3;
+    INPUT_SIZE     : integer := 8;
+    IN_DELAY       : time := 0 ns;
+    LAT            : integer := 2;
+    LAYER          : integer := 0;
+    MAX_MEM_SIZE   : integer := 12;
+    MEM_SIZE       : integer := 12;
+    N_CHANNEL      : integer := 3;
+    N_FILTER       : integer := 16;
+    N_LAYER        : integer := 1;
+    OP_TYPE        : string := "C";
+    PATH           : string := "";
+    RISE_START     : time := 0 ns;
+    RST_TIME       : time := 2.5 ns;
+    SHIFT          : integer := 8;
+    STRIDE         : integer := 1;
+    TOTAL_OPS      : integer := 0;
+    X_SIZE         : integer := 32
+    );
+end tb;
+
+architecture a1 of tb is
+  signal clock, reset, start_conv, debug : std_logic := '0';
+
+  signal ofmap_valid, ofmap_ce, ofmap_we, iwght_ce, iwght_valid, ifmap_ce, ifmap_valid, end_conv : std_logic := '0';
+
+  signal iwght_address, ifmap_address, ofmap_address : std_logic_vector(MEM_SIZE-1 downto 0);
+
+  signal iwght_value, ifmap_value : std_logic_vector((INPUT_SIZE*2)-1 downto 0);
+
+  signal ofmap_out, ofmap_in : std_logic_vector(((INPUT_SIZE*2)+CARRY_SIZE)-1 downto 0);
+
+  signal iwght_n_read, iwght_n_write, ifmap_n_read, ifmap_n_write, ofmap_n_read, ofmap_n_write : std_logic_vector(31 downto 0);
+
+  file sim_file : text open write_mode is "sim.txt";
+  signal gold : type_array_int := read_data(PATH & "/s.txt");
+
+begin
+
+  IWGHT : entity work.memory
+    generic map(
+      ROM => PATH & "g.txt",
+      INPUT_SIZE => INPUT_SIZE*2,
+      ADDRESS_SIZE => MEM_SIZE,
+      DATA_AV_LATENCY => LAT
+      )
+    port map(
+      clock    => clock,
+      reset    => reset,
+      chip_en  => iwght_ce,
+      wr_en    => '0',
+      data_in  => (others => '0'),
+      address  => iwght_address,
+      data_av  => iwght_valid,
+      data_out => iwght_value,
+      n_read   => iwght_n_read,
+      n_write  => iwght_n_write
+      );
+
+  IFMAP : entity work.memory
+    generic map(
+    ROM => PATH & "d.txt",
+      INPUT_SIZE => INPUT_SIZE*2,
+      ADDRESS_SIZE => MEM_SIZE,
+      DATA_AV_LATENCY => LAT
+      )
+    port map(
+      clock    => clock,
+      reset    => reset,
+      chip_en  => ifmap_ce,
+      wr_en    => '0',
+      data_in  => (others => '0'),
+      address  => ifmap_address,
+      data_av  => ifmap_valid,
+      data_out => ifmap_value,
+      n_read   => ifmap_n_read,
+      n_write  => ifmap_n_write
+      );
+
+  OFMAP : entity work.memory
+    generic map(
+      ROM => "no",
+      INPUT_SIZE => ((INPUT_SIZE*2)+CARRY_SIZE),
+      ADDRESS_SIZE => MEM_SIZE,
+      DATA_AV_LATENCY => LAT
+      )
+    port map(
+      clock    => clock,
+      reset    => reset,
+      chip_en  => ofmap_ce,
+      wr_en    => ofmap_we,
+      data_in  => ofmap_out,
+      address  => ofmap_address,
+      data_av  => ofmap_valid,
+      data_out => ofmap_in,
+      n_read   => ofmap_n_read,
+      n_write  => ofmap_n_write
+      );
+
+  DUT : entity work.convolution
+    generic map(
+      N_FILTER       => N_FILTER,
+      N_CHANNEL      => N_CHANNEL,
+      X_SIZE         => X_SIZE,
+      FILTER_WIDTH   => FILTER_WIDTH,
+      CONVS_PER_LINE => CONVS_PER_LINE,
+      MEM_SIZE       => MEM_SIZE,
+      INPUT_SIZE     => INPUT_SIZE,
+      SHIFT          => SHIFT,
+      CARRY_SIZE     => CARRY_SIZE
+      )
+    port map(
+      clock         => clock,
+      reset         => reset,
+
+      start_conv    => start_conv,
+      end_conv      => end_conv,
+      debug         => debug,
+
+      iwght_valid   => iwght_valid,
+      iwght_value   => iwght_value,
+      iwght_address => iwght_address,
+      iwght_ce      => iwght_ce,
+
+      ifmap_valid   => ifmap_valid,
+      ifmap_value   => ifmap_value,
+      ifmap_address => ifmap_address,
+      ifmap_ce      => ifmap_ce,
+
+      ofmap_valid   => ofmap_valid,
+      ofmap_in      => ofmap_in,
+      ofmap_out     => ofmap_out,
+      ofmap_address => ofmap_address,
+      ofmap_we      => ofmap_we,
+      ofmap_ce      => ofmap_ce
+      );
+
+  clock <= not clock after CLK_HALF;
+
+  reset <= '1', '0' after RST_TIME;
+
+  start_conv <= '0', '1' after RST_TIME, '0' after RST_TIME + CLK_HALF;
+
+  process(clock)
+
+    -- convolution counter
+  variable cont_conv           : integer := 0;
+  variable out_line            : line;
+  variable sim_start           : time := 0 ns;
+  variable elapsed             : time;
+  variable total_cycles        : integer := 0;
+  variable iwght_active_cycles : integer := 0;
+  variable ifmap_active_cycles : integer := 0;
+  variable ofmap_active_cycles : integer := 0;
+  variable counting            : boolean := false;
+
+  begin
+
+    if clock'event and clock = '0' then
+      if start_conv = '1' then
+        sim_start := now;
+        counting  := true;
+      end if;
+
+      if counting then
+        total_cycles := total_cycles + 1;
+        if iwght_ce = '1' then
+          iwght_active_cycles := iwght_active_cycles + 1;
+        end if;
+        if ifmap_ce = '1' then
+          ifmap_active_cycles := ifmap_active_cycles + 1;
+        end if;
+        if ofmap_ce = '1' then
+          ofmap_active_cycles := ofmap_active_cycles + 1;
+        end if;
+      end if;
+
+      if debug = '1' and cont_conv < TOTAL_OPS then
+        if ofmap_out /= CONV_STD_LOGIC_VECTOR(gold(CONV_INTEGER(unsigned(ofmap_address))), ((INPUT_SIZE*2)+CARRY_SIZE)) then
+          report "end of simulation with error!";
+          report "number of convolutions executed: " & integer'image(cont_conv);
+          report "idx: " & integer'image(CONV_INTEGER(unsigned(ofmap_address)));
+          report "expected value: " & integer'image(gold(CONV_INTEGER(unsigned(ofmap_address))));
+
+          if (INPUT_SIZE*2)+CARRY_SIZE > 32 then
+            report "obtained value: " & integer'image(CONV_INTEGER(ofmap_out(31 downto 0)));
+          else
+            report "obtained value: " & integer'image(CONV_INTEGER(ofmap_out));
+          end if;
+
+          assert false severity failure;
+        end if;
+        cont_conv := cont_conv + 1;
+
+      elsif end_conv = '1' then
+        counting := false;
+        report "number of iwght read: " & integer'image(CONV_INTEGER(unsigned(iwght_n_read)));
+        report "number of iwght write: " & integer'image(CONV_INTEGER(unsigned(iwght_n_write)));
+        report "number of ifmap read: " & integer'image(CONV_INTEGER(unsigned(ifmap_n_read)));
+        report "number of ifmap write: " & integer'image(CONV_INTEGER(unsigned(ifmap_n_write)));
+        report "number of ofmap read: " & integer'image(CONV_INTEGER(unsigned(ofmap_n_read)));
+        report "number of ofmap write: " & integer'image(CONV_INTEGER(unsigned(ofmap_n_write)));
+        report "number of convolutions: " & integer'image(cont_conv);
+
+        elapsed := now - sim_start;
+        write(out_line, string'("runtime="));
+        write(out_line, elapsed);
+        writeline(sim_file, out_line);
+
+        write(out_line, string'("total_cycles="));
+        write(out_line, integer'image(total_cycles));
+        writeline(sim_file, out_line);
+
+        write(out_line, string'("iwght_active_cycles="));
+        write(out_line, integer'image(iwght_active_cycles));
+        writeline(sim_file, out_line);
+
+        write(out_line, string'("ifmap_active_cycles="));
+        write(out_line, integer'image(ifmap_active_cycles));
+        writeline(sim_file, out_line);
+
+        write(out_line, string'("ofmap_active_cycles="));
+        write(out_line, integer'image(ofmap_active_cycles));
+        writeline(sim_file, out_line);
+
+        report "total_cycles=" & integer'image(total_cycles);
+        report "iwght_active_cycles=" & integer'image(iwght_active_cycles);
+        report "ifmap_active_cycles=" & integer'image(ifmap_active_cycles);
+        report "ofmap_active_cycles=" & integer'image(ofmap_active_cycles);
+
+        report "end of simulation without error!" severity failure;
+      end if;
+    end if;
+
+  end process;
+
+end a1;
