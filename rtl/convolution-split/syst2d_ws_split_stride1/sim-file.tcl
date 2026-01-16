e8e5ea6d446aeebb2b65912c11191262dbc5b52e
Társio Onofrio <5504075+tarsioonofrio@users.noreply.github.com>
2026-01-15 23:11:41 -0300

    Rename sim_file.tcl to sim-file.tcl and update sim results
    Uncomment PATH assignment in sim-file.tcl to use the local simulation
    data
    directory. Update sim.txt with the new runtime and reset cycle counts to
    zero.

--- rtl/convolution-split/syst2d_ws_split_stride1/sim-file.tcl
+++ rtl/convolution-split/syst2d_ws_split_stride1/sim-file.tcl
@@ -9,7 +9,7 @@ set LAYER_PATH "../../../experiments/rtl_output/$CNN/$RTL/layer/$LAYER"
 set GENERIC_FILE "$LAYER_PATH/generic_file.txt"
 set PATH $LAYER_PATH
 set LAT 1
-# set PATH "/home/tarsio/gaph/FastConv_SystemVerilog/data/ifn9/sim/sim-032-3-3-normal"
+set PATH "/home/tarsio/gaph/FastConv_SystemVerilog/data/ifn9/sim/sim-032-3-3-normal"
 
 # Packages for CNN layer simulation
 # vcom -work work $LAYER_PATH/ifmap_pkg.vhd
