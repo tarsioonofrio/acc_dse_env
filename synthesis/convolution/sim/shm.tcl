a4c8375e23886fa7736d1161c015a844bbf1380e
Társio Onofrio <5504075+tarsioonofrio@users.noreply.github.com>
2026-01-22 16:19:46 -0300

    Update shm.tcl

--- synthesis/convolution/sim/shm.tcl
+++ synthesis/convolution/sim/shm.tcl
@@ -1,5 +1,6 @@
 ;# cria/define a pasta dut.shm como default
-database -open waves -shm -into dut.shm -default
+# database -open waves -shm -into dut.shm -default
+database -open waves -evcd -into waves.evcd -default
 
 ;# se você tem arrays grandes, isso evita "not probed" por limite
 # antes do probe
@@ -7,7 +8,8 @@ set ::env(SHM_UNPACKED_LIMIT) 5000000
 
 
 ;# forma mais robusta: sonda tudo que for "top" (evita errar caminho /tb/dut)
-probe -create [scope -tops] -all -depth all
+# probe -create [scope -tops] -all -depth all
+probe -create :tb -all -depth all
 
 run
 exit
